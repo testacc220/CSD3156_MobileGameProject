cdefbf065b0a8f1fdf0e072cc0eea4eedc167939
KuroNoSekai <junkang.ong@digipen.edu>
2025-02-21 15:26:48 +0800

    Initializing Physics System World

---
+++ core/src/main/kotlin/com/testacc220/csd3156_mobilegameproject/Logic/PhysicsEngine.kt
@@ -0,0 +1,64 @@
+import com.badlogic.gdx.math.Vector2
+import com.badlogic.gdx.physics.box2d.Body
+import com.badlogic.gdx.physics.box2d.BodyDef
+import com.badlogic.gdx.physics.box2d.CircleShape
+import com.badlogic.gdx.physics.box2d.Contact
+import com.badlogic.gdx.physics.box2d.ContactImpulse
+import com.badlogic.gdx.physics.box2d.ContactListener
+import com.badlogic.gdx.physics.box2d.Fixture
+import com.badlogic.gdx.physics.box2d.FixtureDef
+import com.badlogic.gdx.physics.box2d.Manifold
+import com.badlogic.gdx.physics.box2d.World
+
+
+class ListenerClass : ContactListener {
+    override fun endContact(contact: Contact) {
+    }
+
+    override fun beginContact(contact: Contact) {
+    }
+
+    override fun preSolve(contact: Contact?, oldManifold: Manifold?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun postSolve(contact: Contact?, impulse: ContactImpulse?) {
+        TODO("Not yet implemented")
+    }
+
+
+}
+
+class PhysicsEngine {
+    private val world: World = World(Vector2(0f, -10f), true)
+
+
+    private fun init()
+    {
+        world.setContactListener(ListenerClass())
+    }
+
+    private fun update()
+    {
+        world.step(1/60f, 6, 2)
+    }
+
+    private fun createBody(pos: Vector2)
+    {
+        val bodyDef :BodyDef = BodyDef()
+        bodyDef.type = BodyDef.BodyType.DynamicBody
+        bodyDef.position.set(pos)
+
+        val body : Body = world.createBody(bodyDef)
+
+        val circle : CircleShape = CircleShape()
+        circle.radius = 6f
+
+        val fixtureDef : FixtureDef = FixtureDef()
+        fixtureDef.shape = circle
+
+        val fixture : Fixture = body.createFixture(fixtureDef)
+
+        circle.dispose()
+    }
+}
